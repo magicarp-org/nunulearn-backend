71bea28b1991c0c235937c5dff5c17dafb7ca078
Chanwoo Kim <chanwookim@me.com>
2022-06-29 04:04:36 +0900

    springboot project init

---
+++ src/main/java/me/chanwookim/nunulearnbackend/NunulearnBackendApplication.java
@@ -0,0 +1,13 @@
+package me.chanwookim.nunulearnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunulearnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NunulearnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/chanwookim/nunulearnbackend/NunulearnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chanwookim.nunulearnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunulearnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
